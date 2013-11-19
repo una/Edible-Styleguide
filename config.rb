cdbf18d0824b537461b3001269ff2b851bf0e349
Una Kravets <Una21136@yahoo.com>
2013-11-19 18:39:26 -0500

    Configured Compass file

--- config.rb
+++ config.rb
@@ -1,20 +1,21 @@
 # Require any additional compass plugins here.
+require "breakpoint"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
